b0bc89f4baec998657416bfbd8a5b0fbfda17c0b
melanie.garnier <melanie.mc.garnier@gmail.com>
2017-12-15 16:35:09 +0100

    merging the two bash scripts into only one run_naccess.sh

--- data/run_naccess.sh
+++ data/run_naccess.sh
@@ -21,8 +21,10 @@ export LC_ALL=fr_FR.UTF-8
 fileName=`basename $targetPdbFile | perl -pe 's/\.inp$//'`
 rsaFile=$fileName".rsa"
 
-# cp $WORKDIR/$rsaFile ./ # for web services
-
+if [ ! -f $rsaFile ]
+then 
+	cp $WORKDIR/$rsaFile ./ # for web services
+fi
 
 # parse & get results
 echo `echo "{\"accessibilities\":[" ;grep "^RES" $rsaFile | awk 'BEGIN{FS="";OFS=""}{print "[\"" $5$6$7 "\",\"" $9 "\"," $10$11$12$13 ", {\"All_atoms\":{\"abs\":" $15$16$17$18$19$20$21$22 ",\"rel\":" $24$25$26$27$28 "}}],"}' | head --bytes -2; echo "]}"`
